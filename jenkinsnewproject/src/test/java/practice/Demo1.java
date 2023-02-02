0dea3e14db12690f4ab0910193f3ef5dfca58ed2
vivekgithubacc <vlvekchoudhary109@gmail.com>
2023-02-02 16:40:23 +0530

    1st commit

---
+++ jenkinsnewproject/src/test/java/practice/Demo1.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1{
+	@Test
+ public void t1()
+ {
+	 System.out.println("hii guys");
+ }
+}
---
+++ jenkinsnewproject/src/test/java/practice/Demo2.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	 public void t2()
+	 {
+		 System.out.println("hii guys");
+	 }
+}
